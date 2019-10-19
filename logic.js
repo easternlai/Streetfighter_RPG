08d5c7d85ddbaf72059041de5ed74c44ec123b68
Eastern Lai <easternlai@hotmail.com>
2019-10-19 09:47:48 -0700

    finished javascript and some css

--- logic.js
+++ logic.js
@@ -3,32 +3,39 @@ var fighters = [
     name: "Ken", 
     health: 100,
     attack: 25,
-    counter: 10,
-    images: ["assets/img/ken.png","assets/img/ken-defeated.png"]     
+    counter: 25,
+    images: ["assets/img/ken.png","assets/img/ken-defeated.png", "assets/img/ken-.png"]     
     },
     { 
     name: "Ryu",
-    health: 120,
+    health: 100,
     attack: 25,
-    counter: 10,
-    images: ["assets/img/ryu.png","assets/img/ryu-defeated.png"] 
+    counter: 25,
+    images: ["assets/img/ryu.png","assets/img/ryu-defeated.png", "assets/img/ryu-.png"] 
     },
     {
      name: "Guile",
      health: 100,
      attack: 25,
-     counter: 10,   
-     images: ["assets/img/guile.png","assets/img/guile-defeated.png"] 
-    }     
+     counter: 25,   
+     images: ["assets/img/guile.png","assets/img/guile-defeated.png", "assets/img/guile-.png"] 
+    },     
+    {
+        name: "Blanka",
+        health: 150,
+        attack: 25,
+        counter: 25,   
+        images: ["assets/img/blanka.png","assets/img/blanka-defeated.png", "assets/img/blanka.png"] 
+       }     
 ]
 
 var gameStarted = false;
 var opponentNeeded = true;
-
-var playerHealth = 0;
-var opponentHealth = 0;
 var userPlayer = " ";
+var userHealthReset;
 var opponent;
+var gameOver = false;
+
 
 function start (){
     $(".user-message").text("Pick your player");    
@@ -43,47 +50,111 @@ $(document).ready(function() {
             var userThat = this;
             playerSelect(userThat);
         }
-        else if(opponentNeeded===true){
-            var oppThat = this;
-            setOpponent(oppThat);
-        }
-       
-    
 
     });
+
+    $("#attack").on("click", function (){
+        if(gameStarted===true && gameOver === false){
+            fight();
+        }
+
+    });
+
 });
 
 function playerSelect(that){
     userPlayer = $(that).val();
-    $(that).attr("class", "hide-icon");
-    var selectedIMG = $("<img>");
-    $(selectedIMG).attr("src", "assets/img/" + userPlayer + ".png");
-    $(".chosen-player").append(selectedIMG);
+
 
     for (i=0; i<fighters.length; i++){
         if (fighters[i].name === userPlayer){
             userPlayer = fighters[i];
+            userHealthReset = fighters[i].health;
             //changes user player name from string to fighter object
             fighters.splice(i,1);
             //removes user selected player from possible opponents
         }
     }
+    $(that).attr("class", "hide-icon");
+    var selectedIMG = $("<img>");
+    $(selectedIMG).attr("src", userPlayer.images[0]);
+    $(selectedIMG).attr("class", "icon");
+    $(".chosen-player").html(selectedIMG);
+    $(".user-message").text("You are " + userPlayer.name);
+    setOpponent();
     gameStarted=true;
 }
 
-function setOpponent(that){
-    opponent = $(that).val(); 
-    $(that).attr("class", "hide-icon");
-    var selectedIMG = $("<img>");
-    $(selectedIMG).attr("src", "assets/img/" + opponent + ".png");
-    $(".chosen-opponent").append(selectedIMG);
+function setOpponent(){
+    userPlayer.health = userHealthReset;
+    console.log(userPlayer.health);
+    opponent= fighters[0];
+    $("#" + opponent.name).attr("class", "hide-icon");
 
     for (i=0; i<fighters.length; i++){
-        if(fighters[i].name === opponent){
-            opponent = fighters[i];
+        if(fighters[i].name === opponent.name){
             fighters.splice(i,1);
         }
     }
-    opponentNeeded = false;    
+
+    displayHealth();
+    removeOpponent();
 }
 
+function fight(){
+    displayHealth();
+    opponent.health -= Math.floor(Math.random() *userPlayer.attack);
+    
+    if(opponent.health <= 0){
+
+        $("#" + opponent.name +"-defeat").attr("class", "unhide-icon");
+        if (fighters.length > 0){
+            setOpponent();
+        }
+        else {
+            var selectedIMG = $("<img>");
+            $(selectedIMG).attr("src", userPlayer.images[0]);
+            $(selectedIMG).attr("class", "hidden-icon");
+
+            $(".chosen-opponent").html(selectedIMG);
+            $(".user-message").text("You win");
+            gameOver=true;
+        }
+    }
+    else{
+        if(opponent.health > 0){
+            var counterDamage = Math.floor(Math.random() *opponent.counter);
+            userPlayer.health -= counterDamage;
+            console.log(counterDamage);
+        }    
+        if(userPlayer.health <=0){
+            $(".user-message").text("You lose");
+            var selectedIMG = $("<img>");
+            $(selectedIMG).attr("src", userPlayer.images[1]);
+            $(".chosen-player").html(selectedIMG);
+            gameOver=true;
+        }
+    }
+    displayHealth();
+}
+
+function removeOpponent(){
+    var selectedIMG = $("<img>");
+    $(selectedIMG).attr("src", opponent.images[0]);
+    $(selectedIMG).attr("class", "icon");
+    $(".chosen-opponent").html(selectedIMG);
+}
+
+function displayHealth (){
+    if(userPlayer.health < 0){
+        $("#player-health").text("0");
+    }else {
+        $("#player-health").text(userPlayer.health);
+    }
+
+    if(opponent.health < 0){
+    $("#opponent-health").text("0");
+    }else {
+        $("#opponent-health").text(opponent.health);    
+    }
+}
