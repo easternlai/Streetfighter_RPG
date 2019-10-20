1738337bc3c9bfb76d2bbb2c0618cef49b46b901
Eastern Lai <easternlai@hotmail.com>
2019-10-19 18:35:56 -0700

    Version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
 # Streetfighter_RPG
+
+##By Eastern Lai
+
+##Tecnologies Used
+
+1. HTML
+2. CSS
+3. Github
+4. visual studio code
+5. Git Bash
+6. JQuery
+7. JavaScript 
+
+
+Functions used
+
+```
+function setOpponent(){
+
+    //resets users health so that user's health is always max at beginning of fight.
+    userPlayer.health = userHealthReset;
+    
+    //sets opponent to first remaining fighter and hides icon from available fighters.
+    opponent= fighters[0];
+    $("#" + opponent.name).attr("class", "hide-icon");
+
+    //removes selected opponent from array.
+    for (i=0; i<fighters.length; i++){
+        if(fighters[i].name === opponent.name){
+            fighters.splice(i,1);
+        }
+    }
+
+    displayHealth();
+
+    //removes opponent from visibility in available fighters.
+    removeOpponent();
+}
+
+function fight(){
+    displayHealth();
+
+    //randomizes hitpoint values.
+    opponent.health -= Math.floor(Math.random() *userPlayer.attack);
+    
+    if(opponent.health <= 0){
+
+        $("#" + opponent.name +"-defeat").attr("class", "unhide-icon");
+        if (fighters.length > 0){
+            setOpponent();
+        }
+        else {
+            var selectedIMG = $("<img>");
+            $(selectedIMG).attr("src", userPlayer.images[0]);
+            $(selectedIMG).attr("class", "hidden-icon");
+
+            $(".chosen-opponent").html(selectedIMG);
+            $(".user-message").text("You win");
+
+            //ends game so user cannot continue pressing attack.
+            gameOver=true;
+        }
+    }
+    else{
+        if(opponent.health > 0){
+
+            //randomizes hitpoint values.
+            var counterDamage = Math.floor(Math.random() *opponent.counter);
+            userPlayer.health -= counterDamage;
+            console.log(counterDamage);
+        }    
+        if(userPlayer.health <=0){
+            $(".user-message").text("You lose");
+            var selectedIMG = $("<img>");
+            $(selectedIMG).attr("src", userPlayer.images[1]);
+            $(".chosen-player").html(selectedIMG);
+
+            //ends game so user cannot continue pressing attack.
+            gameOver=true;
+        }
+    }
+    displayHealth();
+}
+
+function removeOpponent(){
+    var selectedIMG = $("<img>");
+    $(selectedIMG).attr("src", opponent.images[0]);
+    $(selectedIMG).attr("class", "icon");
+    $(".chosen-opponent").html(selectedIMG);
+}
+
+function displayHealth (){
+
+    //fixed bug that shows user with negative health when losing match.
+    if(userPlayer.health < 0){
+        $("#player-health").text("0");
+    }else {
+        $("#player-health").text(userPlayer.health);
+    }
+
+    //fixes bug that displays opponent with negative health.
+    if(opponent.health < 0){
+    $("#opponent-health").text("0");
+    }else {
+        $("#opponent-health").text(opponent.health);    
+    }
+}
+
+```
--- logic.js
+++ logic.js
@@ -36,7 +36,7 @@ var userHealthReset;
 var opponent;
 var gameOver = false;
 
-
+//function send user message
 function start (){
     $(".user-message").text("Pick your player");    
 }
@@ -45,7 +45,7 @@ start();
 
 $(document).ready(function() {
     $(".unchosen-fighter").on("click", function() {
-
+        //user picks player for first time.
         if(gameStarted===false){
             var userThat = this;
             playerSelect(userThat);
@@ -75,22 +75,32 @@ function playerSelect(that){
             //removes user selected player from possible opponents
         }
     }
+
+    //sets selected player icon display property to "none" to hide icon.  
     $(that).attr("class", "hide-icon");
     var selectedIMG = $("<img>");
     $(selectedIMG).attr("src", userPlayer.images[0]);
     $(selectedIMG).attr("class", "icon");
     $(".chosen-player").html(selectedIMG);
     $(".user-message").text("You are " + userPlayer.name);
+
+    //sets opponent
     setOpponent();
+
+    //googles gameStarted to true so that user is never prompted to pick player again.
     gameStarted=true;
 }
 
 function setOpponent(){
+
+    //resets users health so that user's health is always max at beginning of fight.
     userPlayer.health = userHealthReset;
-    console.log(userPlayer.health);
+    
+    //sets opponent to first remaining fighter and hides icon from available fighters.
     opponent= fighters[0];
     $("#" + opponent.name).attr("class", "hide-icon");
 
+    //removes selected opponent from array.
     for (i=0; i<fighters.length; i++){
         if(fighters[i].name === opponent.name){
             fighters.splice(i,1);
@@ -98,11 +108,15 @@ function setOpponent(){
     }
 
     displayHealth();
+
+    //removes opponent from visibility in available fighters.
     removeOpponent();
 }
 
 function fight(){
     displayHealth();
+
+    //randomizes hitpoint values.
     opponent.health -= Math.floor(Math.random() *userPlayer.attack);
     
     if(opponent.health <= 0){
@@ -118,11 +132,15 @@ function fight(){
 
             $(".chosen-opponent").html(selectedIMG);
             $(".user-message").text("You win");
+
+            //ends game so user cannot continue pressing attack.
             gameOver=true;
         }
     }
     else{
         if(opponent.health > 0){
+
+            //randomizes hitpoint values.
             var counterDamage = Math.floor(Math.random() *opponent.counter);
             userPlayer.health -= counterDamage;
             console.log(counterDamage);
@@ -132,6 +150,8 @@ function fight(){
             var selectedIMG = $("<img>");
             $(selectedIMG).attr("src", userPlayer.images[1]);
             $(".chosen-player").html(selectedIMG);
+
+            //ends game so user cannot continue pressing attack.
             gameOver=true;
         }
     }
@@ -146,12 +166,15 @@ function removeOpponent(){
 }
 
 function displayHealth (){
+
+    //fixed bug that shows user with negative health when losing match.
     if(userPlayer.health < 0){
         $("#player-health").text("0");
     }else {
         $("#player-health").text(userPlayer.health);
     }
 
+    //fixes bug that displays opponent with negative health.
     if(opponent.health < 0){
     $("#opponent-health").text("0");
     }else {
